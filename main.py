da67401d678e3ade9ad8eefb0fd862fef042c9cd
statelinecat <statelinecat@gmail.com>
2024-10-28 15:27:17 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,11 +3,18 @@ from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from config import TOKEN
+import random
 # from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 bot = Bot(token=TOKEN)
 # storage = MemoryStorage()  # Добавим хранилище
 dp = Dispatcher()
+
+@dp.message(F.photo)
+async def hiphoto(message: Message):
+    list = ['Ого! Какое фото!', 'Что это?', 'Хулиганим?']
+    await message.answer(random.choice(list))
+    # await message.answer("Ого! Какое фото!")
 
 @dp.message(F.text == 'Привет!')
 async def hitext(message: Message):
