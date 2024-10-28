72692f80810bd812be245e12f4549090cf19dc0f
statelinecat <statelinecat@gmail.com>
2024-10-28 14:47:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,21 @@
 import asyncio
-from aiogram import Bot, Dispatcher
+from aiogram import Bot, Dispatcher, F
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 from config import TOKEN
+# from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 bot = Bot(token=TOKEN)
+# storage = MemoryStorage()  # Добавим хранилище
 dp = Dispatcher()
+
+@dp.message(F.text == 'Привет!')
+async def hitext(message: Message):
+    await message.answer("Привет! Я сообщаю информацию о погоде. Также умею выполнять команды: \n /start \n /help")
 
 @dp.message(Command('help'))
 async def cmd_help(message: Message):
-    await message.answer("Я умею выполнять команды:\\n/start\\n/help")
+    await message.answer("Я умею выполнять команды: \n /start \n /help")
 @dp.message(CommandStart())
 async def cmd_start(message: Message):
     await message.answer(f"Привет, {message.from_user.full_name}!")
